e13fc4e0d6778b96afd296ab3c979c7b1d713cf6
Manoel Pedro <manoelpedromn@hotmail.com>
2026-02-03 13:16:39 -0300

    feat: ajustei o componente de header com um link em volta da imagem refund, e ajuste na page de confirm

--- src/pages/Confirm.tsx
+++ src/pages/Confirm.tsx
@@ -1,28 +1,33 @@
-import { useNavigate, useLocation, Navigate } from "react-router"
+import { useLocation, Navigate } from "react-router"
 
-import Button from "../components/Button"
-import okImg from "../assets/icons/ok.svg"
+import okSvg from "../assets/icons/ok.svg"
 
 function Confirm() {
-  const navigate = useNavigate()
   const location = useLocation()
+
   if (!location.state?.fromSubmit) {
     return <Navigate to="/" />
   }
 
   return (
-    <div className="w-full flex flex-col bg-white p-10 rounded-2xl">
-      <div className="text-center mb-10">
-        <h1 className="text-green-100 text-center font-bold text-2xl">Solicitação enviada!</h1>
+    <div className="w-full flex flex-col gap-6 items-center bg-white p-10 rounded-2xl">
+      <h1 className="text-green-100 text-center font-bold text-2xl">
+        Solicitação enviada!
+      </h1>
 
-        <img src={okImg} alt="image de okay taokay" className="w-30 my-6 mx-auto" />
+      <img src={okSvg} alt="image de okay taokay" className="w-28" />
 
-        <p className="text-sm text-center font-medium text-gray-200">
-          Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o setor
-          financeiro irá entrar em contato com você.
-        </p>
-      </div>
-      <Button onClick={() => navigate("/")}>Nova Solicitação</Button>
+      <p className="text-sm text-center font-medium text-gray-200">
+        Agora é apenas aguardar! Sua solicitação será analisada e, em breve, o setor
+        financeiro irá entrar em contato com você.
+      </p>
+
+      <a
+        href="/"
+        className="w-full bg-green-100 hover:bg-green-200 text-white text-center rounded-2xl p-3 transition ease-linear hover:scale-103"
+      >
+        Nova Solicitação
+      </a>
     </div>
   )
 }

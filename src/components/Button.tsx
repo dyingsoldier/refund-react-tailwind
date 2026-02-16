e0b2b64990fdf3a55531f70c22d65666a231a64b
Manoel Pedro <manoelpedromn@hotmail.com>
2026-02-16 17:01:08 -0300

    ajust de estilização em button variant e no scroll global

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,12 @@ import classMerge from "../utils/classMerge"
 
 type ButtonProps = React.ComponentProps<"button"> & {
   isLoading?: boolean
-  variant?: "base" | "md" | "sm"
+  variant?: "icon" | "sm" | "md" | "base"
 }
 
 const ButtonVariant = {
   variants: {
+    icon: "h-8 w-8",
     sm: "h-10 w-10",
     md: "h-12 w-12",
     base: "h-12 w-full",
@@ -25,7 +26,7 @@ function Button({
     <button
       disabled={isLoading}
       className={classMerge([
-        "flex items-center justify-center bg-green-100 text-white p-1 rounded-2xl cursor-pointer outline-none transition ease-linear hover:bg-green-200 hover:scale-103 disabled:opacity-50 disabled:cursor-progress",
+        "flex items-center justify-center bg-green-100 text-white p-1 rounded-xl cursor-pointer outline-none transition ease-linear hover:bg-green-200 hover:scale-103 disabled:opacity-50 disabled:cursor-progress",
         ButtonVariant.variants[variant],
         className,
       ])}

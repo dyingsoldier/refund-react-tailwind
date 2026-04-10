f9643c4047f34ad5d5e77b4ae745338081700b0a
Manoel Pedro <manoelpedromn@hotmail.com>
2026-04-10 08:05:12 -0300

    buscando da api os refunds e exibindo na role de manager

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,14 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import Input from "../components/Input"
 import Button from "../components/Button"
 import Pagination from "../components/Pagination"
-
 import RefundItem, { type RefundItemProps } from "../components/RefundItem"
 import searchSVG from "../assets/icons/search.svg"
 
+import { AxiosError } from "axios"
+
+import { api } from "../services/api"
 import { CATEGORIES } from "../utils/category"
 import { formatCurrency } from "../utils/formatCurrency"
 import { useAuth } from "../hooks/useAuth"
@@ -34,22 +36,39 @@ const refundExample = [
     icon: CATEGORIES["services"].icon,
   },
 ]
+const PER_PAGE = 5
 
 function Dashboard() {
   const [name, setName] = useState("")
   const [page, setPage] = useState(1)
-  const [totalOfPages, setTotalPages] = useState(10)
+  const [totalOfPages, setTotalPages] = useState(5)
 
   // Puxando Dados do usuario pelo contexto global.
   const context = useAuth()
   console.log(context.session?.user.name)
 
-  const [refunds, setRefunds] = useState<RefundItemProps[]>([refundExample[0]])
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([
+    refundExample[0],
+    refundExample[1],
+    refundExample[2],
+  ])
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault()
+  async function fetchRefunds() {
+    try {
+      const response = await api.get(
+        `/refunds?name=${name.trim()}&page=${page}&perPage${PER_PAGE}`,
+      )
 
-    console.log(name)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message)
+      }
+
+      return alert("Não foi Possivel identificar o erro.")
+    }
   }
 
   function handlePagination(action: "next" | "prev") {
@@ -64,12 +83,16 @@ function Dashboard() {
     })
   }
 
+  useEffect(() => {
+    fetchRefunds()
+  }, [])
+
   return (
     <div className="bg-gray-500 rounded-xl p-10 lg:w-lg">
       <h1 className="text-gray-100 font-bold text-xl">Solicitações</h1>
 
       <form
-        onSubmit={onSubmit}
+        onSubmit={fetchRefunds}
         className="flex flex-1 justify-between items-center mt-6 pb-6 border-b border-b-gray-400 md:flex-row gap-1.5"
       >
         <Input
